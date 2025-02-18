2d27c30fa321e6c02b6b8acd4dac8578c60557c0
unknown <shubhamdadas095@gmail.com>
2025-02-18 11:51:08 +0530

    Resolved merge conflicts in package.json and package-lock.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
-import * as React from 'react';
-import Component from './Component';
-import './App.css';
+import * as React from "react";
+import UsersList from "./components/pages/UserList/UserList";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Component/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>
-          <h1>Hello</h1>
-        </div>
-      </header>
+      <UsersList />
     </div>
   );
 }
